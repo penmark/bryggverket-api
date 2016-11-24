e7f84c32271e2267c36ad75efbd423fad2f916a6
Pontus Enmark <pontus.enmark@umu.se>
2016-11-24 08:27:36 +0100

    Remove debug logging

--- src/routes/instagram.ts
+++ src/routes/instagram.ts
@@ -1,5 +1,4 @@
 import { instagram } from 'instagram-node';
-import { logger } from '../server';
 import { Router } from 'express';
 
 const ig = instagram();
@@ -10,10 +9,8 @@ latest.get('/', (req, res, next) => {
   const count = req.query.count || 9;
   ig.user_self_media_recent({count}, (err, medias, pagination, remaining, limit) => {
     if (err) {
-      logger.error(err);
       return res.send(err.body)
     }
-    logger.debug('media', {numMedias: medias.length, pagination, remaining, limit});
     res.send(medias)
   })
 });
